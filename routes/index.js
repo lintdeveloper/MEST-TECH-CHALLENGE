baf666d6b907888ccbcb92043b7a31233afa40c5
Musa Musa <musabrillz@gmail.com>
2019-02-27 20:59:21 +0100

    Added GET /add-to-cart/:id route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var Cart = require('../models/cart');
+const { House } = require('../models/house');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
  res.render('index')
+});
+
+router.get('/add-to-cart/:id', function (req, res) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart.items : {});
+
+    House.findById(productId, function (err, house) {
+        cart.add(house, house.id);
+        req.session.cart = cart;
+        // console.log(req.session.cart);
+        
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
