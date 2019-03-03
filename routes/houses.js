3af0c3c7ad61ac85f26b161a0ba2c45c9b8e5e7c
Musa Musa <musabrillz@gmail.com>
2019-03-04 00:00:43 +0100

    Implemented the /GET houses/update route and protect route

--- routes/houses.js
+++ routes/houses.js
@@ -16,8 +16,9 @@ router.get('/', function (req, res) {
 });
 
 /** GET /houses/new  */
-router.get("/new", (req, res)=>{
-    res.render('houses/new');
+router.get("/new", ensureAuthenticated, (req, res)=>{
+    let user = req.user;
+    res.render('houses/new', {user: user});
 });
 
 /** GET /houses/:id */
@@ -28,32 +29,36 @@ router.get('/:id', (req, res)=>{
 });
 
 /** GET /houses/update */
-router.get('/:id/update', (req, res)=>{
-    House.findById(req.params.id).then((house)=>{
-        console.log(house);
-        res.render('houses/update',{house: house})
+router.get('/:id/update', ensureAuthenticated, (req, res)=>{
+    let id = req.params.id;
+    console.log(req.user);
+    
+    House.find({_id: id}, (err, houses)=>{  
+        res.render('houses/update', {house: houses[0], user: req.user})
     });
 });
 
-/** PUT /houses/:id */
-router.put('/:id', (req, res)=>{
+/** Updates the route /houses/:id */
+router.post('/update', ensureAuthenticated, (req, res)=>{
+    let oldName = req.body.old_id,
+        newName = req.body.picture_id;
+        
     cloudinary.v2.uploader.rename(oldName, newName,
         (error, result) => {
             if (error) res.send(error);
-            House.findOneAndUpdate({ image_id: oldName },
-                Object.assign({}, req.body, { image: result.url }),
+            House.findOneAndUpdate({ picture_id: oldName },
+                Object.assign({}, req.body, { picture: result.url }),
                 function (err) {
                     if (err) res.send(err);
-
-                    res.redirect('/');
-                })
+                    res.redirect('/dashboard');
+            })
         })
 });
 
 /** GET /houses */
 
 /** DELETE /houses/:id */
-router.post('/delete', (req, res)=>{
+router.post('/delete', ensureAuthenticated, (req, res)=>{
     let pictureId = req.body.picture_id;
     cloudinary.v2.uploader.destroy(pictureId, (error, result) => {
         House.findOneAndDelete({ picture_id: pictureId }, (err) => {
